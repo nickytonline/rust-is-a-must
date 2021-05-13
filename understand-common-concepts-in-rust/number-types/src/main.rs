be8420d27cee8904e51066ad282d6cc73c7f4952
Nick Taylor <nick@iamdeveloper.com>
2021-05-12 23:14:00 -0400

    Number types exercise.

---
+++ understand-common-concepts-in-rust/number-types/src/main.rs
@@ -0,0 +1,17 @@
+fn main() {
+    // Addition
+    println!("1 + 2 = {}", 1u32 + 2);
+
+    // Subtraction
+    println!("1 - 2 = {}", 1i32 - 2);
+    // ^ Try changing `1i32` to `1u32` to see why the type is important
+
+    // Integer Division
+    println!("9 / 2 = {}", 9u32 / 2);
+
+    // Float Division
+    println!("9 / 2 = {}", 9.0 / 2.0);
+
+    // Multiplication
+    println!("3 * 6 = {}", 3 * 6)
+}
